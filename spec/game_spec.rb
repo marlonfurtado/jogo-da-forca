28f95f702df332b11efc8ec798b746e3ac205e37
marlonfurtado <marlonfurtado.rs@gmail.com>
2017-02-18 16:14:38 -0200

    Preparar cenario

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,16 +1,33 @@
 require 'spec_helper'
-require './app/game'
+require './lib/game'
 
 describe Game do
+  let(:output) { double("output") }
+  subject(:game) { Game.new(output) }
+
   describe "#start" do
     it "prints the initial message" do
-      output = double("output")
-      game = Game.new(output)
-
       initial_message = "Bem vindo ao jogo da forca!"
-      output.should_receive(:puts).with(initial_message)
+      expect(output).to receive(:puts).with(initial_message)
 
       game.start
+    end
+  end
+
+  describe "#ended?" do
+    it "returns false when the game just started" do
+      expect(game).to_not be_ended
+    end
+  end
+
+  describe "#next_step" do
+    context "when the game just started" do
+      it "asks the player for the length of the word to be raffled" do
+        question = "Qual o tamanho da palavra a ser sorteada?"
+        expect(output).to receive(:puts).with(question)
+
+        game.next_step
+      end
     end
   end
 end
